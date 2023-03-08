7eed5a22c0eb11f26188af2ca4bce880e7112109
birdwatcherYT <aabcehmu.362880@hotmail.co.jp>
2023-03-09 06:15:39 +0900

    resolve warn

--- src/solver.ts
+++ src/solver.ts
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction, } from 'react';
+import { Dispatch, SetStateAction, } from 'react';
 import PriorityQueue from "./priorityQueue";
 import ZobristHashing from "./zobristHashing";
 import { sleep } from "./tools";
